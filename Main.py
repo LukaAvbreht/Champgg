d10e2e6acfced343c14e3b22a562b963cbc68146
LukaAvbreht <l.avbreht@gmail.com>
2016-02-15 12:57:45 +0100

    popravlej readme, iz spletne strani pridobimo slovar z podatki

--- Main.py
+++ Main.py
@@ -1,15 +1,22 @@
 __author__ = 'Luka'
 import Pridobivanje
+import json
+import re
 
-Pridobivanje.shrani('http://champion.gg/statistics/#?sortBy=general.winPercent&order=descend&roleSort=','champgg.txt')
-sez = dict()
+Pridobivanje.shrani('http://champion.gg/statistics/#?sortBy=general.winPercent&order=descend&roleSort=','champgg.html')
 
-with open('champgg.txt', encoding="UTF8") as vhod:
-    print('karkoli')
+in_file = open("champgg.html")
 
+data = in_file.read()
 
+regex = """<script>\s*matchupData.stats = (?P<podatki>.*);\s*</script>"""
 
+a = re.search(regex, data)
 
+podatki = a.groupdict()["podatki"]
 
+moji = json.loads(podatki)
 
+print(moji[0])
 
+print(moji[0].keys())
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Champgg
+
+Projekt pri predmetu Programiranje 1 namenjen primerjavi razlicnih karaterjev v priljubljeni racunalniski igri League of legends
+
+## Kaj primerjamo
+
+Primerjave med procentom zmaganih iger in zlatom ki ga zasluzijo, povprecnim stevilom smrti, ubojev...
+
+## Viri
+https://champion.gg/
